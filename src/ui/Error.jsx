ebf03890d1486a47fe684d74bbbf8af63c847ae7
Dinesh <dineshsdk1978@gmail.com>
2025-07-02 14:50:47 +0530

    add styles in error page

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -5,10 +5,14 @@ function Error() {
   const error = useRouteError();
   console.log(error);
   return (
-    <div>
-      <h1>Something went wrong 😢</h1>
-      <p>{error.data || error.message}</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+    <div className="flex items-center justify-center flex-col min-h-screen gap-2 ">
+      <h1 className="font-semibold text-stone-500 sm:font-extrabold text-2xl sm:text-3xl">
+        Something went wrong 😢
+      </h1>
+      <p className="text-indigo-400 text-sm">{error.data || error.message}</p>
+      <button onClick={() => navigate(-1)} className="text-blue-300 text-xl ">
+        &larr; Go back
+      </button>
     </div>
   );
 }
